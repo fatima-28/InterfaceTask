5e2bc4355f58de49f62911a19a0b9dc722f9afd7
fatima-28 <2n6gddb@code.edu.az>
2022-04-03 22:09:27 +0400

    task added

---
+++ interfaceTask/interfaceTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Fatima Bayramova", "fatimabayramova2002@gmail.com");
+            user1.PasswordChecker("2002Fatima");
+            user1.ShowInfo();
+        }
+        public interface IAccount
+        {
+            public bool PasswordChecker(string password);
+            public void ShowInfo();
+        }
+
+    }
+}
---
+++ interfaceTask/interfaceTask/User.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace classwork
+{
+    public class User : IAccount
+    {
+        public string Fullname;
+        public string Email;
+        public string Password;
+        public User()
+        {
+            Console.WriteLine("Email yaratmaqiniz mecburidir!");
+        }
+        public User(string fullname, string email)
+        {
+            Email = email;
+            Fullname = fullname;
+
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            bool IsPassword = false;
+            int TrueCount = 0;
+            if (password.Length >= 8)//8li sifre 1 boy 1 kicik herf 1 reqem
+            {
+                for (int i = 0; i < password.Length; i++)
+                {
+                    char character = password[i];
+                    int digit = password[i];
+                    if (password[i] == Char.ToUpper(character))
+                    {
+
+                        TrueCount++;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid password");
+                        break;
+                    }
+
+                    if (password[i] == Char.ToLower(character))
+                    {
+
+                        TrueCount++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid password");
+                        break;
+                    }
+                    if (password[i] == digit)
+                    {
+                        TrueCount++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid password");
+                        break;
+                    }
+                }
+            }
+            if (TrueCount >= 3)
+            {
+                IsPassword = true;
+            }
+            else
+            {
+                IsPassword = false;
+            }
+
+            return IsPassword;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"User fullname: {Fullname} , User email: {Email}");
+        }
+    }
+}
